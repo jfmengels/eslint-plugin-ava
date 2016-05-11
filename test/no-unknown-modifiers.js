a03bd28375ad6f934b7ac61e1836ae3f18766680
Jeroen Engels <jfm.engels@gmail.com>
2016-05-11 23:42:06 +0200

    Get nice feedback with RuleTester using AVA

--- test/no-unknown-modifiers.js
+++ test/no-unknown-modifiers.js
@@ -2,6 +2,15 @@ import test from 'ava';
 import {RuleTester} from 'eslint';
 import rule from '../rules/no-unknown-modifiers';
 
+RuleTester.describe = function (text, method) {
+	RuleTester.it.validity = text;
+	return method.apply(this);
+};
+
+RuleTester.it = function (text, method) {
+	test(RuleTester.it.validity + ': ' + text, method);
+};
+
 const ruleTester = new RuleTester({
 	env: {
 		es6: true
@@ -11,72 +20,70 @@ const ruleTester = new RuleTester({
 const ruleError = {ruleId: 'no-unknown-modifiers'};
 const header = `const test = require('ava');\n`;
 
-test(() => {
-	ruleTester.run('no-unknown-modifiers', rule, {
-		valid: [
-			`${header} test(t => {});`,
-			`${header} test.after(t => {});`,
-			`${header} test.afterEach(t => {});`,
-			`${header} test.before(t => {});`,
-			`${header} test.beforeEach(t => {});`,
-			`${header} test.cb(t => {});`,
-			`${header} test.cb.only(t => {});`,
-			`${header} test.only(t => {});`,
-			`${header} test.serial(t => {});`,
-			`${header} test.skip(t => {});`,
-			`${header} test.todo(t => {});`,
-			// shouldn't be triggered since it's not a test file
-			`test.foo(t => {});`
-		],
-		invalid: [
-			{
-				code: `${header} test.foo(t => {});`,
-				errors: [
-					{...ruleError, message: 'Unknown test modifier `foo`.'}
-				]
-			},
-			{
-				code: `${header} test.onlu(t => {});`,
-				errors: [
-					{...ruleError, message: 'Unknown test modifier `onlu`.'}
-				]
-			},
-			{
-				code: `${header} test.beforeeach(t => {});`,
-				errors: [
-					{...ruleError, message: 'Unknown test modifier `beforeeach`.'}
-				]
-			},
-			{
-				code: `${header} test.c.only(t => {});`,
-				errors: [
-					{...ruleError, message: 'Unknown test modifier `c`.'}
-				]
-			},
-			{
-				code: `${header} test.cb.onlu(t => {});`,
-				errors: [
-					{...ruleError, message: 'Unknown test modifier `onlu`.'}
-				]
-			},
-			{
-				code: `${header} test.foo.bar.baz(t => {});`,
-				errors: [
-					{...ruleError, message: 'Unknown test modifier `foo`.'}
-				]
-			},
-			{
-				code: `${header} test.default(t => {});`,
-				errors: [
-					{...ruleError, message: 'Unknown test modifier `default`.'}
-				]
-			},
-			{
-				code: `${header} test.test(t => {});`,
-				errors: [
-					{...ruleError, message: 'Unknown test modifier `test`.'}
-				]
-			}
-		]
-	});
+ruleTester.run('no-unknown-modifiers', rule, {
+	valid: [
+		`${header} test(t => {});`,
+		`${header} test.after(t => {});`,
+		`${header} test.afterEach(t => {});`,
+		`${header} test.before(t => {});`,
+		`${header} test.beforeEach(t => {});`,
+		`${header} test.cb(t => {});`,
+		`${header} test.cb.only(t => {});`,
+		`${header} test.only(t => {});`,
+		`${header} test.serial(t => {});`,
+		`${header} test.skip(t => {});`,
+		`${header} test.todo(t => {});`,
+		// shouldn't be triggered since it's not a test file
+		`test.foo(t => {});`
+	],
+	invalid: [
+		{
+			code: `${header} test.foo(t => {});`,
+			errors: [
+				{...ruleError, message: 'Unknown test modifier `foo`.'}
+			]
+		},
+		{
+			code: `${header} test.onlu(t => {});`,
+			errors: [
+				{...ruleError, message: 'Unknown test modifier `onlu`.'}
+			]
+		},
+		{
+			code: `${header} test.beforeeach(t => {});`,
+			errors: [
+				{...ruleError, message: 'Unknown test modifier `beforeeach`.'}
+			]
+		},
+		{
+			code: `${header} test.c.only(t => {});`,
+			errors: [
+				{...ruleError, message: 'Unknown test modifier `c`.'}
+			]
+		},
+		{
+			code: `${header} test.cb.onlu(t => {});`,
+			errors: [
+				{...ruleError, message: 'Unknown test modifier `only`.'}
+			]
+		},
+		{
+			code: `${header} test.foo.bar.baz(t => {});`,
+			errors: [
+				{...ruleError, message: 'Unknown test modifier `foo`.'}
+			]
+		},
+		{
+			code: `${header} test.default(t => {});`,
+			errors: [
+				{...ruleError, message: 'Unknown test modifier `default`.'}
+			]
+		},
+		{
+			code: `${header} test.test(t => {});`,
+			errors: [
+				{...ruleError, message: 'Unknown test modifier `test`.'}
+			]
+		}
+	]
 });
